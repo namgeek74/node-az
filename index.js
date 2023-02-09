0ad0d2ae06703a580fab4b4f6b0bbf7d94a09b54
namnd <namnguyennd7428@gmail.com>
2023-02-09 15:50:01 +0700

    change port default to deploy on render service

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import __dirname from './dirname.js';
 import sampleMethodsRoute from './routes/sampleMethods.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/assets'));
 app.use(express.json());
